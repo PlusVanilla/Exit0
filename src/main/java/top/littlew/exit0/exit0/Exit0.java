b2f3c163f7622bde0e350426286716446231eaf1
LittleChest <hiya@littlew.top>
2024-04-19 02:28:06 +0800

    1.20.4

---
+++ src/main/java/top/littlew/exit0/exit0/Exit0.java
@@ -0,0 +1,14 @@
+package top.littlew.exit0;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.minecraft.client.MinecraftClient;
+
+public class Exit0 implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            MinecraftClient.getInstance().stop();
+        });
+    }
+}
